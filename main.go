901d73bd48c30ec3c6a110baaa1d2e7cb081a086
Jon Vojtush <11096910+JonVojtush@users.noreply.github.com>
2025-01-07 00:45:08 -0700

    debug

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 
@@ -11,14 +13,14 @@ import (
 )
 
 var (
-	err    error
-	tsCode []byte
-	result goja.Value
+	err      error
+	tsCode   []byte
+	result   goja.Value
 	filePath string
 )
 
 func main() {
-	flag.StringVar(&filePath, "file", "/scripts.ts", "path to the TypeScript file")
+	flag.StringVar(&filePath, "file", "scripts.ts", "path to the TypeScript file")
 	flag.Parse()
 
 	if tsCode, err = os.ReadFile(filePath); err != nil {
@@ -26,18 +28,20 @@ func main() {
 		return
 	}
 
-	if result, err = typescript.Evaluate(string(tsCode)); err != nil {
-		fmt.Println("Error evaluating TypeScript:", err)
-		return
+	log.Printf("TypeScript Code: \n%s\n", string(tsCode))
+
+	if result, err = typescript.Evaluate(bytes.NewReader(tsCode)); err != nil {
+		log.Fatalln("Typescript:", err)
 	}
 
 	jsCode := result.String()
 
+	log.Printf("JavaScript Code: \n%s\n", jsCode)
+
 	jsFilePath := filepath.Join(filepath.Dir(filePath), filepath.Base(filePath[:len(filePath)-len(filepath.Ext(filePath))])+".js")
 
 	if err = os.WriteFile(jsFilePath, []byte(jsCode), 0644); err != nil {
-		fmt.Println("Error writing to file:", err)
-		return
+		log.Fatalln("Error writing to file: ", err)
 	}
 
 	fmt.Println("JavaScript file created (or overwritten):", jsFilePath)

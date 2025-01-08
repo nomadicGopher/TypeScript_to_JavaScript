577c5384b78a7348787c6757cf13f9a4aa068258
Jon Vojtush <jonathanvojtush@gmail.com>
2025-01-07 18:04:12 -0700

    add flags & fix transpiler

--- go.mod
+++ go.mod
@@ -1,15 +1,16 @@
-module github.com/nomadicGopher/DirectCMS/interpreters/typescriptToJavascript
+module github.com/nomadicGopher/typescript2javascript
 
-go 1.23.4
+go 1.20
 
 require (
 	github.com/clarkmcc/go-typescript v0.7.0
-	github.com/dop251/goja v0.0.0-20241024094426-79f3a7efcdbd
+	github.com/tdewolff/minify/v2 v2.21.2
 )
 
 require (
-	github.com/dlclark/regexp2 v1.11.4 // indirect
+	github.com/dlclark/regexp2 v1.4.1-0.20201116162257-a2a8dda75c91 // indirect
+	github.com/dop251/goja v0.0.0-20211115154819-26ebff68a7d5 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
-	github.com/google/pprof v0.0.0-20230207041349-798e818bf904 // indirect
-	golang.org/x/text v0.3.8 // indirect
+	github.com/tdewolff/parse/v2 v2.7.19 // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
--- main.go
+++ main.go
@@ -9,39 +9,64 @@ import (
 	"path/filepath"
 
 	"github.com/clarkmcc/go-typescript"
-	"github.com/dop251/goja"
+	"github.com/tdewolff/minify/v2"
+	"github.com/tdewolff/minify/v2/js"
 )
 
 var (
-	err      error
-	tsCode   []byte
-	result   goja.Value
-	filePath string
+	err        error
+	tsCode     []byte
+	jsCode     []byte
+	filePath   string
+	minifyFlag bool
+	strictFlag bool
 )
 
 func main() {
 	flag.StringVar(&filePath, "file", "scripts.ts", "path to the TypeScript file")
+	flag.BoolVar(&minifyFlag, "minify", false, "minify the JavaScript output")
+	flag.BoolVar(&strictFlag, "strict", false, `add "use strict"; to the JavaScript output`)
 	flag.Parse()
 
 	if tsCode, err = os.ReadFile(filePath); err != nil {
-		fmt.Println("Error reading file:", err)
-		return
+		log.Fatalf("Error reading file: %v", err)
 	}
 
-	log.Printf("TypeScript Code: \n%s\n", string(tsCode))
+	// log.Printf("TypeScript Code: \n%s\n----------\n", string(tsCode))
 
-	if result, err = typescript.Evaluate(bytes.NewReader(tsCode)); err != nil {
-		log.Fatalln("Typescript:", err)
+	jsCodeStr, err := typescript.TranspileString(string(tsCode))
+	if err != nil {
+		log.Fatalf("Error transpiling TypeScript: %v", err)
 	}
 
-	jsCode := result.String()
+	if strictFlag {
+		if minifyFlag {
+			jsCodeStr = `"use strict";` + jsCodeStr
+		} else {
+			jsCodeStr = `"use strict";\n` + jsCodeStr
+		}
+	}
+	jsCode = []byte(jsCodeStr)
 
-	log.Printf("JavaScript Code: \n%s\n", jsCode)
+	if minifyFlag {
+		m := minify.New()
+		m.AddFunc("application/javascript", js.Minify)
+		var minified bytes.Buffer
+		if err = m.Minify("application/javascript", &minified, bytes.NewReader(jsCode)); err != nil {
+			log.Fatalf("Minify: %v", err)
+		}
+		jsCode = minified.Bytes()
+	}
 
-	jsFilePath := filepath.Join(filepath.Dir(filePath), filepath.Base(filePath[:len(filePath)-len(filepath.Ext(filePath))])+".js")
+	ext := ".js"
+	if minifyFlag {
+		ext = ".js.min"
+	}
 
-	if err = os.WriteFile(jsFilePath, []byte(jsCode), 0644); err != nil {
-		log.Fatalln("Error writing to file: ", err)
+	jsFilePath := filepath.Join(filepath.Dir(filePath), filepath.Base(filePath[:len(filePath)-len(filepath.Ext(filePath))])+ext)
+
+	if err = os.WriteFile(jsFilePath, jsCode, 0o644); err != nil {
+		log.Fatalf("Error writing to file: %v", err)
 	}
 
 	fmt.Println("JavaScript file created (or overwritten):", jsFilePath)
